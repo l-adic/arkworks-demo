5909398a87fe06359d1cf4103e0c97a1e3d1a609
martyall <martyall@protonmail.com>
2024-07-07 13:41:31 -0700

    works

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,46 @@
-use wasmer::{Memory, MemoryType, Instance, Module, Store};
-use wasmer_wasix::{generate_import_object_from_env, WasiEnv, WasiVersion};
+use ark_circom::{Wasm, WitnessCalculator};
 use color_eyre::Result;
 use num_bigint::BigInt;
-use ark_circom::{Wasm, WitnessCalculator};
-use serde_json::Value;
-use std::{collections::HashMap, str::FromStr};
 use num_traits::Num;
+use serde_json::Value;
+use std::{collections::HashMap, path::PathBuf};
+use wasmer::{Instance, Memory, MemoryType, Module, Store};
+use wasmer_wasix::{generate_import_object_from_env, WasiEnv, WasiVersion};
 
-
-fn main() {
-
+#[tokio::main]
+async fn main() {
+    let wtns = run_example().unwrap();
+    print!("witness {:?}", wtns);
 }
 
-fn run_example() -> Result<()> {
+fn run_example() -> Result<Vec<BigInt>> {
     let mut store = Store::default();
-    let wasm = create_wasm_instance(&mut store)?;
-    let wc = WitnessCalculator::new_from_wasm(&mut store, wasm);
-    Ok(())
-    
+    let wasm = create_wasm_instance(&mut store, PathBuf::from("./circuit-assets"))?;
+    let inputs = parse_inputs("./inputs.json");
+    let mut wc = WitnessCalculator::new_from_wasm(&mut store, wasm)?;
+    wc.calculate_witness(&mut store, inputs, false)
 }
 
-fn parse_inputs(inputs_path: impl AsRef<std::path::Path>) -> HashMap<String, Vec<BigInt>>{
+fn parse_inputs(inputs_path: impl AsRef<std::path::Path>) -> HashMap<String, Vec<BigInt>> {
     let inputs_str = std::fs::read_to_string(inputs_path).unwrap();
     let inputs: std::collections::HashMap<String, serde_json::Value> =
         serde_json::from_str(&inputs_str).unwrap();
+
+    fn value_to_bigint(v: Value) -> BigInt {
+        match v {
+            Value::String(inner) => {
+                BigInt::from_str_radix(&inner.strip_prefix("0x").unwrap(), 16).unwrap()
+            }
+            _ => panic!("unsupported type, requires hex encoded string"),
+        }
+    }
 
     inputs
         .iter()
         .map(|(key, value)| {
             let res = match value {
-                Value::String(inner) => {
-                    vec![BigInt::from_str(inner).unwrap()]
+                Value::String(_) => {
+                    vec![value_to_bigint(value.clone())]
                 }
                 Value::Array(inner) => inner.iter().cloned().map(value_to_bigint).collect(),
                 _ => panic!(),
@@ -41,25 +51,20 @@ fn parse_inputs(inputs_path: impl AsRef<std::path::Path>) -> HashMap<String, Vec
         .collect::<HashMap<_, _>>()
 }
 
-
-fn value_to_bigint(v: Value) -> BigInt {
-     match v {
-         Value::String(inner) => BigInt::from_str_radix(&inner, 16).unwrap(),
-         _ => panic!("unsupported type"),
-     }
- }
-
-fn create_wasm_instance(store: &mut Store) -> Result<Wasm> {
-    let module = Module::from_file(&store, "./circuit-assets/circuit.wasm")?;
-    let wasi_env = 
-          WasiEnv::builder("calculateWitness")
-            .preopen_dir("./circuit-assets")?
-            .finalize(store)?;
+fn create_wasm_instance(store: &mut Store, circuit_dir: PathBuf) -> Result<Wasm> {
+    let module = Module::from_file(&store, circuit_dir.clone().join("circuit.wasm"))?;
+    let host_fs = wasmer_wasix::virtual_fs::host_fs::FileSystem::default();
+    let mut wasi_env = WasiEnv::builder("calculateWitness")
+        .fs(Box::new(host_fs))
+        .preopen_dir("/")?
+        .map_dir("/", circuit_dir)?
+        .finalize(store)?;
     let wasi_env_imports =
         generate_import_object_from_env(store, &wasi_env.env, WasiVersion::Snapshot1);
+    let memory = Memory::new(store, MemoryType::new(2000, None, false)).unwrap();
     let instance = Instance::new(store, &module, &wasi_env_imports)?;
     let exports = instance.exports.clone();
-    let memory = Memory::new(store, MemoryType::new(2000, None, false)).unwrap();
+    wasi_env.initialize_with_memory(store, instance, Some(memory.clone()), false)?;
     let wasm = Wasm::new(exports, memory);
     Ok(wasm)
 }
